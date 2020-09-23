4cc330b9dbeb0289f97d7bd4749260ce84ce80ed
Swiggatron <jjswigut@gmail.com>
2020-09-22 20:01:33 -0400

    Ready

--- app/src/main/java/com/jjswigut/matters/MainActivity.kt
+++ app/src/main/java/com/jjswigut/matters/MainActivity.kt
@@ -2,7 +2,7 @@ package com.jjswigut.matters
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//ready to go
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
